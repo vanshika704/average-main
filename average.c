9bdede27854ee0424a3fab8b60575f28083eb73e
vanshika704 <sharmavanshi704@gmail.com>
2023-12-10 10:00:00 +0530

    temperature change

---
+++ average.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int i;int n;int m;int o; int p;
+    float avg;
+    printf("enter number 1");
+    scanf("%d",&i);
+printf("enter number 2");
+scanf("%d",&n);
+printf("enter number 3");
+scanf("%d",&m);
+    printf("enter number 4");
+     scanf("%d",&o);
+printf("enter number 5");
+ scanf("%d",&p);
+ avg=(float)(i+n+m+o+p)/4;
+ printf("average:%f\n",avg);
+return 0;}
